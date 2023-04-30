fa55050e7c773ee21145cc7b8362e449601cd953
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-04-30 14:50:33 +0200

    basics
    have to do saving and calculation next

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let body = document.getElementsByTagName("body")[0];
+let new_goal_button = document.getElementById("new_goal_button");
+
+function make_goal() {
+    let text = prompt("Write a goal:");
+    if (text == "" || text == undefined) return
+
+    let goal = document.createElement("div");
+
+    goal.textContent = text;
+    goal.classList.add("goal");
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    goal.appendChild(checkbox);
+
+    body.insertBefore(goal, new_goal_button);
+}
