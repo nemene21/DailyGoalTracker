6522196d37678f6f62064b860f7f48f517788d89
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-05-02 16:49:47 +0200

    daily goal bar animation

--- script.js
+++ script.js
@@ -6,15 +6,36 @@ let daily_bar   = document.getElementById("daily_bar");
 let alltime_bar = document.getElementById("alltime_bar");
 let weekly_bar  = document.getElementById("weekly_bar");
 
+function lerp(a, b, c) {
+    return a + (b - a) * c
+}
+
 function calculate_daily() {
     let score = 0;
     let goals = document.getElementsByClassName("goal");
 
-    for (let i = 0; i < goals.lastChild.length; i++) {
-        score += Number(goals[i].value);
+    if (goals.length == 0) { return };
+
+    for (let i = 0; i < goals.length; i++) {
+        if (goals[i].lastChild.checked) score++;
     }
 
-    daily_bar.textContent = String(Math.floor(score / goals.length * 100) + "%")
+    let ratio = score / goals.length;
+    color = "rgb(" + String(94 * ratio) + " " + String(lerp(69, 195, ratio)) + " " + String(lerp(171, 100, ratio)) + ")";
+
+    daily_bar.textContent = String(Math.floor(ratio * 100) + "%");
+
+    let animation = daily_bar.animate([
+        {backgroundColor: daily_bar.style.backgroundColor},
+        {backgroundColor: color}
+    ], {
+        duration: 500,
+        iterations: 1
+    });
+
+    animation.onfinish = function() {
+        daily_bar.style.backgroundColor = color;
+    }
 }
 
 function update_ui() {
@@ -32,8 +53,13 @@ function make_goal() {
 
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.onclick = function() { update_ui() };
 
     goal.appendChild(checkbox);
 
     body.insertBefore(goal, new_goal_button);
+
+    update_ui();
 }
+
+update_ui();
