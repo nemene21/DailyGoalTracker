1328cb8e09948d7e5bf5d56cfabf0db69a3d0a1d
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2023-05-02 12:20:37 +0200

    daily calculation

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 let body = document.getElementsByTagName("body")[0];
 let new_goal_button = document.getElementById("new_goal_button");
 
+let daily_bar   = document.getElementById("daily_bar");
+let alltime_bar = document.getElementById("alltime_bar");
+let weekly_bar  = document.getElementById("weekly_bar");
+
+function calculate_daily() {
+    let score = 0;
+    let goals = document.getElementsByClassName("goal");
+
+    for (let i = 0; i < goals.lastChild.length; i++) {
+        score += Number(goals[i].value);
+    }
+
+    daily_bar.textContent = String(Math.floor(score / goals.length * 100) + "%")
+}
+
+function update_ui() {
+    calculate_daily();
+}
 
 function make_goal() {
     let text = prompt("Write a goal:");
